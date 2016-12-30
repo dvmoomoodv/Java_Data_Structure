6924ec531ce944b4ac769c739a24455a19726e53
KimDae-Hee <anyozcall@naver.com>
2016-12-30 17:34:19 +0900

    2016.12.30
    Java 자료구조 시작

---
+++ list/src/List.java
@@ -0,0 +1,13 @@
+/**
+ * Date : 2016.12.31 ~ 2017.01.03
+ * Time : Every Day 4 Hours
+ * Content : C / C++ -> Java Data Structure
+ * Content : list , Stack , Queue
+ */
+public class List {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
